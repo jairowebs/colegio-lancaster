0beca1c63c77e82020987b702ba743d8770640b1
jairowebs <136769364+jairowebs@users.noreply.github.com>
2023-07-09 13:04:36 -0500

    modified

--- index.js
+++ index.js
@@ -22,3 +22,46 @@ var shareLink = document.getElementById("shareLink");
 shareLink.addEventListener("click", function () {
     // Agregar código para compartir la
 });
+
+document.getElementById("mainButton").addEventListener("click", function() {
+  var dropdown = document.getElementById("dropdown");
+  if (dropdown.style.display === "none") {
+    dropdown.style.display = "block";
+  } else {
+    dropdown.style.display = "none";
+  }
+});
+
+
+function activarModoNocturno() {
+  document.body.classList.toggle("modo-nocturno");
+}
+
+
+function duplicarTamano() {
+  document.body.classList.toggle("duplicar");
+}
+
+
+var lecturaActiva = false;
+var voz;
+ function toggleLecturaPagina() {
+  if (lecturaActiva) {
+    detenerLectura();
+  } else {
+    iniciarLectura();
+  }
+}
+ function iniciarLectura() {
+  var texto = document.body.innerText;
+  voz = new SpeechSynthesisUtterance(texto);
+  voz.lang = "es-ES";
+  voz.pitch = 1;
+  voz.rate = 1;
+  speechSynthesis.speak(voz);
+  lecturaActiva = true;
+}
+ function detenerLectura() {
+  speechSynthesis.cancel();
+  lecturaActiva = false;
+}
