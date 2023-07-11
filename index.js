9c027f994eeeab010ab6c0198b0055324a47af4d
jairowebs <136769364+jairowebs@users.noreply.github.com>
2023-07-11 10:12:46 -0500

    jf

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-var registroBtn = document.getElementById("registroBtn");
-var capaFlotante = document.getElementById("capaFlotante");
-registroBtn.addEventListener("click", function() {
-  capaFlotante.style.display = "block";
-});
- capaFlotante.addEventListener("click", function(event) {
-  if (event.target === capaFlotante) {
-    capaFlotante.style.display = "none";
-  }
- });
- //"tarjetas de pagina de servicio"
-
- var likeBtn = document.getElementById("likeBtn");
-var dislikeBtn = document.getElementById("dislikeBtn");
-var shareLink = document.getElementById("shareLink");
- likeBtn.addEventListener("click", function() {
-  // Agregar código para aumentar el contador de "Me gusta"
-});
- dislikeBtn.addEventListener("click", function() {
-  // Agregar código para aumentar el contador de "No me gusta"
-});
-shareLink.addEventListener("click", function () {
-    // Agregar código para compartir la
-});
-
 document.getElementById("mainButton").addEventListener("click", function() {
   var dropdown = document.getElementById("dropdown");
   if (dropdown.style.display === "none") {
@@ -65,3 +40,8 @@ var voz;
   speechSynthesis.cancel();
   lecturaActiva = false;
 }
+
+
+ function mostrarMensaje() {
+    alert("¡Hola! ING Sandra aun este proyecto no cuenta con la construccion del backend RTA.equipo de desarrollo CODELOGY");
+  }
